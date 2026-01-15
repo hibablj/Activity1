ee3037e1c7a038a579b991fe3c346fda92b95174
Raul Munoz <munozzraul30@gmail.com>
2026-01-15 18:29:10 +0100

    Add business layer: AuthorService and PaperService using repository interfaces

---
+++ src/Service/AuthorService.java
@@ -0,0 +1,20 @@
+package business;
+
+import data.AuthorRepository;
+import model.Author;
+import java.util.List;
+
+public class AuthorService {
+
+    private AuthorRepository repository;
+
+    public AuthorService(AuthorRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(Author author) { repository.save(author); }
+    public void update(Author author) { repository.update(author); }
+    public void delete(int id) { repository.delete(id); }
+    public Author findById(int id) { return repository.findById(id); }
+    public List<Author> findAll() { return repository.findAllByOrderByName(); }
+}
---
+++ src/Service/PaperService.java
@@ -0,0 +1,20 @@
+package business;
+
+import data.PaperRepository;
+import model.Paper;
+import java.util.List;
+
+public class PaperService {
+
+    private PaperRepository repository;
+
+    public PaperService(PaperRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(Paper paper) { repository.save(paper); }
+    public void update(Paper paper) { repository.update(paper); }
+    public void delete(int id) { repository.delete(id); }
+    public Paper findById(int id) { return repository.findById(id); }
+    public List<Paper> findAll() { return repository.findAllByOrderByTitle(); }
+}
